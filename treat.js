d67593975200e1934534f0de5c0d1e4bade09eb8
William Dibbern <gotdibbs@outlook.com>
2015-10-11 22:01:15 -0500

    Initial version

---
+++ treat.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+
+    var next,
+        walk = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
+
+    while (next = walk.nextNode()) {
+        if (/Get Yourself a Little Something/.test(next.textContent) !== false) {
+            next.textContent = 'Treat Yo\'self!';
+        }
+    }
+
+}());
