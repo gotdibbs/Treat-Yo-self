90520d20c0380ea8ec98f04ec89c2ca55eacdee4
William Dibbern <gotdibbs@outlook.com>
2016-04-11 08:50:29 -0500

    Switch to case insensitive matching
    - Fix for updated amazon.com homepage

--- treat.js
+++ treat.js
@@ -5,7 +5,7 @@
         walk = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
 
     while (next = walk.nextNode()) {
-        if (/Get Yourself a Little Something/.test(next.textContent) !== false) {
+        if (/Get Yourself a Little Something/i.test(next.textContent) !== false) {
             next.textContent = 'Treat Yo\'self!';
         }
     }
